1488bf6adbfcda0e402442d8b8d64f9dd14bf666
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-02-04 22:38:34 -0600

    Create Supervisor.cs

---
+++ LectureUnitTesting/UnitTestDemo/PersonClasses/Supervisor.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestDemo.PersonClasses
+{
+    public class Supervisor : Person
+    {
+       public List<Employee> Employees { get; set; }
+    }
+}
