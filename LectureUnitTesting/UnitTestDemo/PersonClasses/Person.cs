59186b11c86d449ed6b88aa43d3a576c7944763a
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-02-04 22:38:07 -0600

    Create Person.cs

---
+++ LectureUnitTesting/UnitTestDemo/PersonClasses/Person.cs
@@ -0,0 +1,9 @@
+using System;
+namespace UnitTestDemo.PersonClasses
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
