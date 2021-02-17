12767b3fa9cee51009d56a90a2e51355a10e13e0
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-02-17 11:31:01 -0600

    Create Person.cs

---
+++ LectureSRP/AfterSRP/Person.cs
@@ -0,0 +1,9 @@
+using System;
+namespace SRP
+{
+    public class Person //this did not change
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
