ff04ee91ac1ab11420270db3da68ad3ca91dd4ed
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-02-24 19:33:21 -0600

    Create EmployeeModel.cs

---
+++ LectureOCP/BeforeOCP/EmployeeModel.cs
@@ -0,0 +1,14 @@
+using System;
+namespace OCP
+{
+    public class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+
+        public bool IsManager { get; set; } = false; //sets default to false
+        public bool IsExecutive { get; set; } = false; 
+
+    }
+}
