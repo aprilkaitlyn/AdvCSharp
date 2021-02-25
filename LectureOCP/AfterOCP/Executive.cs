b97c4665623b545c6c674f5b5483140cce8d0201
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-02-24 20:05:28 -0600

    Create Executive.cs

---
+++ LectureOCP/AfterOCP/Executive.cs
@@ -0,0 +1,10 @@
+using System;
+namespace OCP
+{
+    public class Executive : IPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public IAccounts AccountProcessor { get; set; } = new ExecutiveAccounts();
+    }
+}
