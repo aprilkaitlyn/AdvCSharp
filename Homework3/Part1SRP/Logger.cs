0c8b405f5e6b0f5c3573b3ed57631410098f5704
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-05 22:28:48 -0600

    Create Logger.cs

---
+++ Homework3/Part1SRP/Logger.cs
@@ -0,0 +1,11 @@
+using System;
+namespace hw3part1.Utility
+{
+    public static class Logger
+    {
+        public static void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
