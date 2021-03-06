21afbde5f922ab6d74ccd3588f8a8438b9e246bf
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-05 22:26:56 -0600

    Create CardCharge.cs

---
+++ Homework3/Part1SRP/CardCharge.cs
@@ -0,0 +1,34 @@
+using System;
+using hw3part1.Services;
+
+namespace hw3part1.Model
+{
+    public class CardCharge
+    {
+        public static void Pay(PaymentDetails paymentDetails, Cart cart)
+        {
+            using (var paymentGateway = PaymentCapture.Capture()) //removed "newing up" dependency
+            {
+                try
+                {
+                    paymentGateway.Credentials = "account credentials";
+                    paymentGateway.CardNumber = paymentDetails.CreditCardNumber;
+                    paymentGateway.ExpiresMonth = paymentDetails.ExpiresMonth;
+                    paymentGateway.ExpiresYear = paymentDetails.ExpiresYear;
+                    paymentGateway.NameOnCard = paymentDetails.CardholderName;
+                    paymentGateway.AmountToCharge = cart.TotalAmount;
+
+                    paymentGateway.Charge();
+                }
+                catch (AvsMismatchException ex)
+                {
+                    throw new OrderException("The card gateway rejected the card based on the address provided.", ex);
+                }
+                catch (Exception ex)
+                {
+                    throw new OrderException("There was a problem with your card.", ex);
+                }
+            }
+        }
+    }
+}
