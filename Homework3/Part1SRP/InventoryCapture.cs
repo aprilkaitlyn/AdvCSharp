0b1d0ae0b8bc5025796633618e0ea74966642e48
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-05 22:28:13 -0600

    Create InventoryCapture.cs

---
+++ Homework3/Part1SRP/InventoryCapture.cs
@@ -0,0 +1,13 @@
+using hw3part1.Services;
+namespace hw3part1.Model
+{
+    public class InventoryCapture
+    {
+        public static InventorySystem Capture()
+        {
+            InventorySystem inv = new InventorySystem();
+
+            return inv;
+        }
+    }
+}
