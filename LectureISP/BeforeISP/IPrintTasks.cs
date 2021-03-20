2759661a916855f1ea101a70d4399dc4d7f06d5b
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-20 17:29:05 -0500

    Create IPrintTasks.cs

---
+++ LectureISP/BeforeISP/IPrintTasks.cs
@@ -0,0 +1,11 @@
+namespace ISPLecture
+{
+    public interface IPrintTasks
+    {
+        bool PrintContent(string content);
+        bool ScanContent(string content);
+        bool FaxContent(string content);
+        bool PhotoCopyContent(string content);
+        bool PrintDuplexContent(string content);
+    }
+}
