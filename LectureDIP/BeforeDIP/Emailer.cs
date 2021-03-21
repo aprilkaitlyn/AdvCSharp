1d915c3e2223e97a167a35800c9e9b6389845f0b
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-20 23:55:23 -0500

    Create Emailer.cs

---
+++ LectureDIP/BeforeDIP/Emailer.cs
@@ -0,0 +1,7 @@
+ public class Emailer
+    {
+        public void SendEmail(Person p, string msg)
+        {
+            Console.WriteLine($"Simulating sending an email to {p.EmailAddress}");
+        }
+    }
