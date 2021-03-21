176c9e34b2d3b0ee951bbfff66088686013e2515
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-20 23:54:39 -0500

    Create Chore.cs

---
+++ LectureDIP/BeforeDIP/Chore.cs
@@ -0,0 +1,24 @@
+  public class Chore
+    {
+        public string ChoreName { get; set; }
+        public Person Owner { get; set; }
+        public double HoursWorked { get; private set; }
+        public bool IsComplete { get; private set; }
+
+        public void PerformedChore(double hours)
+        {
+            HoursWorked += hours;
+            Logger log = new Logger();
+            log.Log($"{Owner.FirstName} worked on {ChoreName}");
+        }
+
+        public void CompletedChore()
+        {
+            IsComplete = true;
+            Logger log = new Logger();
+            log.Log($"Completed {ChoreName} and it took {HoursWorked} hours");
+
+            Emailer emailer = new Emailer();
+            emailer.SendEmail(Owner, $"The chore {ChoreName} is complete and took {HoursWorked} hours");
+        }
+    }
