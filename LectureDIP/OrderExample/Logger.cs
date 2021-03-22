49d410643599b60b3d98141732700b989e1cccdc
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-03-22 11:44:14 -0500

    Create Logger.cs

---
+++ LectureDIP/OrderExample/Logger.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DIPLecture2
+{
+    public static class Logger
+    {
+        public static void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
