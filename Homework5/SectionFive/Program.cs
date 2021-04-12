93f4a21a9d4f27f0903d09f6d197c3bde9e79962
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2021-04-12 11:33:46 -0500

    Create Program.cs

---
+++ Homework5/SectionFive/Program.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Threading.Tasks;
+
+namespace AsyncBreakfast //SECTION FIVE ----------------------------------------------------------------------
+{
+    class Program
+    {
+        static async Task Main(string[] args) 
+        {
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffee is ready");
+
+            Task<Egg> eggsTask = FryEggsAsync(2); 
+            Task<Bacon> baconTask = FryBaconAsync(3);
+            Task<Toast> toastTask = ToastBreadAsync(2);
+
+            Toast toast = await toastTask;
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+            Juice oj = PourOJ();
+            Console.WriteLine("oj is ready");
+
+            Egg eggs = await eggsTask;
+            Console.WriteLine("eggs are ready");
+            Bacon bacon = await baconTask;
+            Console.WriteLine("bacon is ready");
+
+            Console.WriteLine("Breakfast is ready!");
+        }
+
+        static async Task<Toast> MakeToastWithButterAndJamAsync(int number) //combines the 3 steps of toast
+        {
+            var toast = await ToastBreadAsync(number);
+            ApplyButter(toast);
+            ApplyJam(toast);
+
+            return toast;
+        }
+
+        //This code has still not been updated with the new async methods, therefore cannot run
+        private static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+
+        private static void ApplyJam(Toast toast) =>
+           Console.WriteLine("Putting jam on the toast");
+
+        private static Toast ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+
+            Console.WriteLine("Start toasting...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
+
+        private static Bacon FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            Task.Delay(3000).Wait();
+
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+
+            Console.WriteLine("cooking the second side of bacon...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        private static Egg FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
+        }
+    }
+}
